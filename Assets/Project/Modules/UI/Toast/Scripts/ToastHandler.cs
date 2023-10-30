9061c10f156d5826377265deb28a41ee4f9d1abf
gama <gamaroot@gmail.com>
2023-10-29 23:55:17 -0400

    Creating toast system

---
+++ Assets/Project/Modules/UI/Toast/Scripts/ToastHandler.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Game
+{
+    internal class ToastHandler : MonoBehaviour
+    {
+        [SerializeField] private Toast _prefab;
+
+        private Toast _toast;
+
+        internal void Show(string message)
+        {
+            if (this._toast)
+                Destroy(this._toast.gameObject);
+
+            this._toast = Instantiate(this._prefab);
+            this._toast.Message.text = message;
+            this._toast.transform.SetParent(base.transform, false);
+        }
+    }
+}
