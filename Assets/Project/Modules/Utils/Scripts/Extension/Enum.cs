eff2bdb6b5d43bebff137e0e497e9cea3147b1d8
JIMPARK80 <van0711@gmail.com>
2023-11-27 13:11:38 -0500

    make comment for the Enum part

--- Assets/Project/Modules/Utils/Scripts/Extension/Enum.cs
+++ Assets/Project/Modules/Utils/Scripts/Extension/Enum.cs
@@ -2,11 +2,17 @@ using System;
 
 namespace Utils
 {
+    // T must be a value type and implement the IConvertiable interface.
+    // this is to prevent users from passing
+    // incorrect formats and to maintain secure code.
     internal class Enum<T> where T : struct, IConvertible
     {
+
+        // returns the number of elements in the enumeration.
         internal static int Length()
         {
             return Enum.GetValues(typeof(T)).Length;
+            // the length of array is returned
         }
     }
 }
